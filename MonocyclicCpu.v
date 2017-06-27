90bbbc4bae9e2fa6c1b9eb85a374976ff7cc2c55
yangyf0419 <yangyifei_2008@2008.sina.com>
2017-06-27 18:05:48 +0800

    finished single cycle CPU structure, debug needed

--- MonocyclicCpu.v
+++ MonocyclicCpu.v
@@ -1,113 +1,156 @@
-//单周期整体部分
+//MonocyclicCpu.v
 `timescale 1ns/1ps
 
 module MonocyclicCpu (reset, clk);
-	input reset;
-	input clk;
+    input reset;
+    input clk;
 
-	reg [31:0] PC;
-	wire [31:0] PC_next;
-	always @(posedge clk or  negedge reset) begin
-		if (~reset)
-			PC <= 32'h00000000;
-		else 
-			PC <= PC_next;
-	end
+    reg [31:0] PC;
+    wire [31:0] PC_next;
+    always @(posedge clk or negedge reset) begin
+        if (~reset)
+            PC <= 32'h00000000;
+        else 
+            PC <= PC_next;
+    end
 
-	
-	wire [31:0] Instruction;
-	InstructionMemory instruction_memory1(.Address(PC),.Instruction(Instruction));
+    // instruction memory part
+    wire [31:0] Instruction;
+    ROM rom(
+        .Address(PC),
+        .Instruction(Instruction));
 
-	wire [2:0] PCSrc;
-	wire [1:0] RegDst;
-	wire RegWr;
-	wire ALUSrc1;
-	wire ALUSrc2;
-	wire ALUFun;
-	wire Sign;
-	wire MemWrite;
-	wire MemRead;
-	wire [1:0] MemtoReg;	
-	wire ExtOp;
-	wire LUOp;
-	wire [31:0] ALUOut;
+    wire [31:0] DataBusA;
+    wire [31:0] DataBusB;
+    wire [31:0] DataBusC;
 
-	Control control1(
-		.OpCode(Instruction[31:26]), .Funct(Instruction[5:0]),
-		.PCSrc(PCSrc), .ALUSrc1(ALUSrc1), .ALUSrc2(ALUSrc2), .RegDst(RegDst),
-		.RegWr(RegWr), .ALUFun(ALUFun), .Sign(Sign), .MemWrite(MemWrite),
-		.MemRead(MemRead), .MemtoReg(MemtoReg), .ExtOp(ExtOp),
-		.LUOp(LUOp), .ALUOut(ALUOut)  );
+    wire [4:0] Rd;
+    wire [4:0] Rt;
+    wire [4:0] Rs;
+    wire [4:0] Shamt;
+    wire [15:0] Imm16;
+    wire [27:0] JT;
+    wire [5:0] OpCode;
+    wire [5:0] Funct;
 
-	wire [31:0] DataBusA;
-	wire [31:0] DataBusB;
-	wire [31:0] DataBusC;
-	wire [4:0] AddrC;
-	wire [4:0] Xp;		//这里指定为常量26，即发生中断或者异常时，返回地址将保存到$26寄存器中
-	wire [4:0] Ra;
-	wire [4:0] Rd;
-	wire [4:0] Rt;
-	wire [4:0] Rs;
-	wire [4:0] Shamt;		//位移量
-	wire [15:0] Imm16;		//16位的立即数
-	wire [25:0] JT;		//跳转到指定地址
+    assign Rd = Instruction[15:11];
+    assign Rt = Instruction[20:16];
+    assign Rs = Instruction[25:21];
+    assign Imm16 = Instruction[15:0];
+    assign JT = {Instruction[25:0], 2'b00};
+    assign Shamt = Instruction[10:6];
+    assign OpCode = Instruction[31:26];
+    assign Funct = Instruction[5:0];
 
-	assign JT = Instruction[25:0];
-	assign Imm16 = Instruction[15:0];
-	assign Rd = Instruction[15:11];
-	assign Rt = Instruction[20:16];
-	assign Rs = Instruction[25:21];
-	assign Shamt = Instruction[10:6];
-	Xp <= 5'b11010;
-	Ra <= 5'b11111;
+    parameter Xp = 5'd26; // exception register
+    parameter Ra = 5'd31; // function breakpoint register
 
-	//Register FIle的Addrc
-	assign AddrC = 
-		(RegDst == 2'b00)? Rd : 
-		(RegDst == 2'b01)? Rt :
-		(RegDst == 2'b10)? Ra :
-		Xp;
+    // control part
+    wire [2:0] PCSrc;
+    wire [1:0] RegDst;
+    wire RegWrite;
+    wire ALUSrc1;
+    wire ALUSrc2;
+    wire [5:0] ALUFun;
+    wire Sign;
+    wire MemWrite;
+    wire MemRead;
+    wire [1:0] MemtoReg;
+    wire ExtOp;
+    wire LUOp;
 
-	RegisterFile register_file1(
-		.WrC(RegWr), .AddrC(AddrC), .AddrA(Rs), 
-		.AddrB(Rt), .ReadDataA(DataBusA), .ReadDataB(DataBusB),
-		.WriteDataC(DataBusC)   );
+    Control ctrl(
+        .OpCode(OpCode),
+        .Funct(Funct),
+        .PCSrc(PCSrc),
+        .ALUSrc1(ALUSrc1),
+        .ALUSrc2(ALUSrc2),
+        .RegDst(RegDst),
+        .RegWrite(RegWrite),
+        .ALUFun(ALUFun),
+        .Sign(Sign),
+        .MemWrite(MemWrite),
+        .MemRead(MemRead),
+        .MemtoReg(MemtoReg),
+        .ExtOp(ExtOp),
+        .LUOp(LUOp));
 
-	wire [31:0] Imm_deal;		//ALUSrc2对应的选择器的1输入口
-	wire [31:0] ext_imm;		//EXT32的输出
-	assign Imm_deal = (LUOp)? {Imm16 , 16'b0} : ;
+    // register part
+    wire [4:0] AddrC;
+    assign AddrC = 
+        (RegDst == 2'b00)? Rd : 
+        (RegDst == 2'b01)? Rt :
+        (RegDst == 2'b10)? Ra :
+        Xp;
 
-	//ALU的两个输入
-	wire [31:0] A;
-	wire [31:0] B;
+    RegFile rgf(
+        .reset(reset),
+        .clk(clk),
+        .addr1(Rs),
+        .data1(DataBusA),
+        .addr2(Rt),
+        .data2(DataBusB),
+        .wr(RegWrite),
+        .addr3(AddrC),
+        .data3(DataBusC));
 
-	assign A = (ALUSrc1)? {27'b0 , Shamt} : DataBusA;
-	assign B = (ALUSrc2)?;
+    // immediate number process parts
+    wire [31:0] Ext_out; // output of EXTOp module
+    assign Ext_out = {ExtOp? {16{Imm16[15]}} : 16'h0000, Imm16};
 
-	//与PC相关的输入
-	wire [31:0] PC_plus_4;
-	wire ComBA;		//PC+4 与指令中 16 位立即数左移 2 位后的数值之和，对应branch
-	wire DataBusA;		//$ra
-	wire ILLOP;		//发生中断时，ILLOP（常量 0x80000004）
-	wire XADR;		//发生异常时，XADR（常量 0x80000008）
-	wire linkRegister;		//jalr的跳转地址
-	wire [31:0] PC_deal;		//由ALUOut信号控制的选择器的输出
+    wire [31:0] LU_out; // output of LUOp mux
+    assign LU_out = (LUOp)? {Imm16, 16'b0} : Ext_out;
 
-	ILLOP <= 32'h80000004;
-	XADR <= 32'h80000008;
+    // input of ALU
+    wire [31:0] input_A;
+    wire [31:0] input_B;
 
-	
-	assign PC_plus_4 = PC + 32'd4;
-	assign PC_deal = (ALUOut[0])? ComBA : PC_plus_4;
+    assign input_A = (ALUSrc1)? {27'b0, Shamt} : DataBusA;
+    assign input_B = (ALUSrc2)? LU_out : DataBusB;
 
-	assign PC_next = 
-		(PCSrc == 3'b000)? PC_plus_4 :
-		(PCSrc == 3'b001)? PC_deal :
-		(PCSrc == 3'b010)? JT :
-		(PCSrc == 3'b011)? DataBusA :
-		(PCSrc == 3'b100)? ILLOP :
-		(PCSrc == 3'b101)? XADR : 
-		(PCSrc == 3'b110)? linkRegister : 
-		32'h00000000;
+    // program counter part
+    wire [31:0] PC_plus_4;
+    wire ConBA;
+    parameter ILLOP = 32'h80000004; // Interruption
+    parameter XADR = 32'h80000008; // Exception
+    wire [31:0] Branch; // output of ALUout[0] mux
+
+    assign PC_plus_4 = PC + 32'd4;
+    assign Branch = (ALUOut[0])? ConBA : PC_plus_4;
+    assign ConBA = PC_plus_4 + {Ext_out[29:0], 2'b00};
+
+    assign PC_next = 
+        (PCSrc == 3'b000)? PC_plus_4 :
+        (PCSrc == 3'b001)? Branch :
+        (PCSrc == 3'b010)? JT :
+        (PCSrc == 3'b011)? DataBusA :
+        (PCSrc == 3'b100)? ILLOP :
+        (PCSrc == 3'b101)? XADR : 
+        32'h00000000;
+
+    // alu part
+    wire [31:0] ALUOut;
+    ALU alu(
+    	.A(input_A),
+    	.B(input_B),
+    	.ALUFun(ALUFun),
+    	.Sign(Sign),
+    	.Z(ALUOut));
+
+    // data memory part
+    wire [31:0] MemData;
+    DataMem mem(
+    	.reset(reset),
+    	.clk(clk),
+    	.rd(MemRead),
+    	.wr(MemWrite),
+    	.addr(ALUout),
+    	.wdata(DataBusB),
+    	.rdata(MemData));
+
+    assign DataBusC = 
+    	(MemtoReg[1:0] == 2'b00)? ALUout :
+    	(MemtoReg[1:0] == 2'b01)? MemData :
+    	PC_plus_4;
 
 endmodule
