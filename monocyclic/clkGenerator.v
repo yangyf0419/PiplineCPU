4a9a1cb3e7c64fafb3875d949aa43191e7670c28
yangyf0419 <yangyifei_2008@2008.sina.com>
2017-07-07 00:19:40 +0800

    resolve clock logics

--- monocyclic/clkGenerator.v
+++ monocyclic/clkGenerator.v
@@ -3,24 +3,26 @@
 // CPU clk generator
 module clkGenerator (sysclk,reset,clk);
 input sysclk,reset;
-output reg clk;
+output reg clk; //////////////////////////////////////////////////////////////////////////////////////////////////////
 integer counter;
 
 initial begin
     counter = 0;
-    clk <= 0;
+    clk <= 0; ///////////////////////////////////////////////////////////////////////////////////////////////////////
 end
 
-always @(posedge sysclk or negedge reset) begin
+// output clk;
+// assign clk = sysclk;
+always @(posedge sysclk or negedge reset) begin /////////////////////////////////////////////////////////////////////
     if(~reset)  begin
         counter = 0;
     end
     else begin
-        if(counter == 50)    begin
+        if(counter == 4) begin
             clk <= ~clk;
         end
-        if(counter != 50)    counter += 1;
+        if(counter != 4)    counter <= counter + 1;
         else    counter = 0;
     end
-end
+end ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 endmodule
