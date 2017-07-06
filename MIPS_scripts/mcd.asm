cb0d220ae2d525892d4c88fda84a0e3a3fdf5cd4
yangyf0419 <yangyifei_2008@2008.sina.com>
2017-07-07 00:17:33 +0800

    modify MIPS codes

--- MIPS_scripts/mcd.asm
+++ MIPS_scripts/mcd.asm
@@ -2,18 +2,14 @@
 # get the maximum common divisor of two 8-bit numbers
 
 #---- Register Contents ----#
-# $v0(2) - Result
 # $a0(4) - First Input Number
 # $a1(5) - Second Input Number
 #
-# $t6(14) - 
-# $t7(15) - 
 # $s3(19) - A Mask to Reset Digitube - 8'h00000800
 # $s4(20) - A Mask to Control Digitube - 8'h00000080
-# $s5(21) - A Mask to Set TCON[1:0] to 11 - 8'h00000011
+# $s5(21) - A Mask to Set TCON[1:0] to 11 - 8'h00000003
 # $s6(22) - A Mask to Set UART_CON[0] to 1 - 8'h00000001
-# $s7(23) - A Mask to Set UART_CON[1]/TCON[1] to 1 - 8'h00000010
-# $t8(24) - 
+# $s7(23) - A Mask to Set UART_CON[1]/TCON[1] to 1 - 8'h00000002
 # $t9(25) - Base of Peripheral - 8'h40000000
 # $k0(26) - Next PC of Interruption / Exception
 #----------- end -----------#
@@ -33,6 +29,7 @@ j initial
 j interruption
 j exception
 
+begin:
 sw $s7, 32($t9) # enable UART_RX
 get_first:
 lw $t0, 32($t9) # get UART_CON
@@ -41,6 +38,7 @@ beq $t1, $zero, get_first # if !RX_Status, keep on
 andi $t0, $t0, -4
 sw $t0, 32($t9) # stop receiving
 lw $a0, 28($t9) # first Number
+# line 10
 andi $t0, $a0, 15 # low 4 bit of first number
 sw $t0, 128($zero)
 srl $t0, $a0, 4 # high 4 bit of first number
@@ -53,6 +51,7 @@ andi $t1, $t0, 8 # get UART_CON[3]
 beq $t1, $zero, get_second # if !RX_Status, keep on
 andi $t0, $t0, -4
 sw $t0, 32($t9) # stop receiving
+# line 20
 lw $a1, 28($t9) # second number
 andi $t0, $a1, 15 # low 4 bit of second number
 sw $t0, 512($zero)
@@ -60,13 +59,14 @@ srl $t0, $a1, 4 # high 4 bit of second number
 sw $t0, 1024($zero)
 
 # start the timer
-addi $t0, $zero, 256
+addi $t0, $zero, -5000000
 sw $t0, 0($t9)
 addi $t0, $zero, -1
 sw $t0, 4($t9)
 sw $s5, 8($t9)
 
 loop:
+# line 30
 beq $a0, $a1, output
 sub $t0, $a0, $a1
 bltz $t0, less
@@ -83,70 +83,73 @@ sw $a0, 12($t9) # ligh up LEDs
 sw $a0, 24($t9)
 sw $s6, 32($t9)
 waiting:
+# line 40
 lw $t0, 32($t9) # get UART_CON
 andi $t1, $t0, 4 # get UART_CON[2]
 beq $t1, $zero, waiting # if !TX_Status, keep on
 sw $t0, 32($t9) # stop sending
-sw $zero, 8($t9) # stop the timer
+
+j begin
 
 
 #----- Initialization ------#
 initial: # setup at the begining of the program
-addi $ra, $zero, 3 # set $ra as the first line of the program
+addi $ra, $zero, 12 # set $ra as the first line of the program
 lui $k1, 32768 # $k1 = 8'h80000000
 lui $t9, 16384 # $t9 = 8'h40000000
-addi $s7, $zero, 2 # $s7 = 8'h00000010
+addi $s7, $zero, 2 # $s7 = 8'h00000002
 addi $s6, $zero, 1 # $s6 = 8'h00000001
-addi $s5, $zero, 3 # $s5 = 8'h00000011
-addi $s4, $zero, 128 # 8'h00000080
+addi $s5, $zero, 3 # $s5 = 8'h00000003
+addi $s4, $zero, 128 # $s4 = 8'h00000080
+addi $s3, $zero, 2048 # $s3 = 8'h00000800
 
 # Use Data Memory as BCD Module
 # 7'b1000000
-addi $t0, $t0, 64
+addi $t0, $zero, 64
 sw $t0, 0($zero)
 # 7'b1111001
-addi $t0, $t0, 121
+addi $t0, $zero, 121
 sw $t0, 4($zero)
 # 7'b0100100
-addi $t0, $t0, 36
+addi $t0, $zero, 36
 sw $t0, 8($zero)
 # 7'b0110000
-addi $t0, $t0, 48
+addi $t0, $zero, 48
 sw $t0, 12($zero)
 # 7'b0011001
-addi $t0, $t0, 25
+addi $t0, $zero, 25
 sw $t0, 16($zero)
 # 7'b0010010
-addi $t0, $t0, 18
+addi $t0, $zero, 18
 sw $t0, 20($zero)
 # 7'b0000010
-addi $t0, $t0, 2
+addi $t0, $zero, 2
 sw $t0, 24($zero)
 # 7'b1111000
-addi $t0, $t0, 120
+addi $t0, $zero, 120
 sw $t0, 28($zero)
 # 7'b0000000
 sw $zero, 32($zero)
 # 7'b0010000
-addi $t0, $t0, 16
+addi $t0, $zero, 16
 sw $t0, 36($zero)
 # 7'b0001000
-addi $t0, $t0, 8
+addi $t0, $zero, 8
 sw $t0, 40($zero)
 # 7'b0000011
-addi $t0, $t0, 3
+addi $t0, $zero, 3
 sw $t0, 44($zero)
 # 7'b1000110
-addi $t0, $t0, 70
+addi $t0, $zero, 70
 sw $t0, 48($zero)
 # 7'b0100001
-addi $t0, $t0, 33
+addi $t0, $zero, 33
 sw $t0, 52($zero)
 # 7'b0000110
-addi $t0, $t0, 6
+addi $t0, $zero, 6
 sw $t0, 56($zero)
 # 7'b0001110
-addi $t0, $t0, 14
+addi $t0, $zero, 14
 sw $t0, 60($zero)
 
 jr $ra # begin the normal program
